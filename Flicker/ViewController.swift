1cb6e7e906573188e42704ac4d515a2597eb71ab
Tegdam <damibf@felix-damibas-mac-mini.local>
2016-01-16 16:47:55 -0500

    Initial Commit

---
+++ Flicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicker
+//
+//  Created by felix damiba on 1/16/16.
+//  Copyright © 2016 felix damiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
